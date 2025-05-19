9fa726fb5c4fe29713906f1c227998dd3973ecfa
Björn Petersen <git@bjoernpetersen.net>
2025-05-19 22:28:35 +0200

    Add span to converter

--- src/bot/conversion.py
+++ src/bot/conversion.py
@@ -2,7 +2,10 @@ import asyncio
 import logging
 from pathlib import Path
 
+from opentelemetry import trace
+
 _LOG = logging.getLogger(__name__)
+_tracer = trace.get_tracer(__name__)
 
 
 class AudioConverter:
@@ -10,28 +13,33 @@ class AudioConverter:
         pass
 
     async def convert_to_wave(self, input_file: Path) -> Path:
-        output_file = input_file.with_suffix(".wav")
-        if output_file == input_file:
-            _LOG.info("Short-circuiting due to input file already having wave format")
-            return input_file
+        with _tracer.start_as_current_span("convert_to_wave"):
+            output_file = input_file.with_suffix(".wav")
+            if output_file == input_file:
+                _LOG.info(
+                    "Short-circuiting due to input file already having wave format"
+                )
+                return input_file
 
-        process = await asyncio.create_subprocess_exec(
-            "ffmpeg",
-            "-i",
-            input_file,
-            output_file,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
+            with _tracer.start_as_current_span("ffmpeg"):
+                process = await asyncio.create_subprocess_exec(
+                    "ffmpeg",
+                    "-i",
+                    input_file,
+                    output_file,
+                    stdout=asyncio.subprocess.PIPE,
+                    stderr=asyncio.subprocess.PIPE,
+                )
 
-        stdout, stderr = await process.communicate()
-        return_code = process.returncode
-        if return_code:
-            _LOG.error(
-                "Converted file with exit code %d",
-                return_code,
-                extra=dict(stdout=stdout, stderr=stderr),
-            )
-            raise OSError("Could not convert file")
+                stdout, stderr = await process.communicate()
 
-        return output_file
+            return_code = process.returncode
+            if return_code:
+                _LOG.error(
+                    "Converted file with exit code %d",
+                    return_code,
+                    extra=dict(stdout=stdout, stderr=stderr),
+                )
+                raise OSError("Could not convert file")
+
+            return output_file
